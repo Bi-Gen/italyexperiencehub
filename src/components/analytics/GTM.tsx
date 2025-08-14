973bda669050f31fb5706c5c3b7907e645d40586
Lorenzo <lorenzosarna@gmail.com>
2025-08-14 09:53:58 +0200

    Add GTM component and fix import path

---
+++ src/components/analytics/GTM.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Script from "next/script";
+
+export default function GTM() {
+  const id = process.env.NEXT_PUBLIC_GTM_ID;
+  if (!id) return null;
+
+  return (
+    <>
+      {/* Consent Mode v2: default denied (prima che GTM parta) */}
+      <Script id="consent-default" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'default', {
+            ad_storage: 'denied',
+            analytics_storage: 'denied',
+            ad_user_data: 'denied',
+            ad_personalization: 'denied',
+            wait_for_update: 500
+          });
+        `}
+      </Script>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${id}');
+        `}
+      </Script>
+    </>
+  );
+}
